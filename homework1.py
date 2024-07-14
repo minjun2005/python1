484813fa157a8555810059779e3860cc92e0456a
minjun2005 <cmjun0722@gmail.com>
2024-07-14 18:47:27 +0900

    homework1

---
+++ homework1.py
@@ -0,0 +1,8 @@
+a = input("학과를 입력하세요:")
+b = input("학번을 입력하세요:")
+c = input("이름을 입력하세요:")
+d = input("거주지를 입력하세요:")
+print(a,b,c,d)
+print('%s %s %s %s' %(a,b,c,d) )
+print("{0},{1},{2},{3}".format(a,b,c,d))
+print(f"{a},{b},{c},{d}")
